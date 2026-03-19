836860100124acf422958ebc6b149d2354209274
gkub <gkubiski@hotmail.com>
2026-03-18 21:27:27 -0600

    Experimenting with freertos queues + intentionally blocking, works nicely with mock data

--- src/main.c
+++ src/main.c
@@ -7,19 +7,34 @@
 #include "app.h"
 #include "i2cscanner.h"
 
-QueueHandle_t sensor_queue;
+
 SemaphoreHandle_t i2c_mutex;
+QueueHandle_t sensor_queue;
+static uint32_t t = 0;
 
 void sensor_task(void *pvParameters) {
     while (1) {
+        sensor_sample_t sample;
+        sample.lux = 100;
+        sample.temperature = 20;
+        sample.humidity = 50;
+        sample.pressure = 1013;
+        sample.timestamp = t;
+        if (xQueueSend(sensor_queue, &sample, 0) != pdTRUE) {
+            printf("Queue full, dropping sample\n");
+        }
         printf("sensor task alive\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
+        t++;
     }
-};
+}
+
 void display_task(void *pvParameters) {
     while (1) {
         printf("display task alive\n");
-        vTaskDelay(pdMS_TO_TICKS(1500));
+        sensor_sample_t sample;
+        xQueueReceive(sensor_queue, &sample, portMAX_DELAY);
+        printf("display task received sample: %f, %f, %f, %f, %lu\n", sample.lux, sample.temperature, sample.humidity, sample.pressure, sample.timestamp);
     }
 }
 
